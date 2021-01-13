1efd3ebb7ead4650e4b32ba7543645f3a8b5da79
ardigermenji <ardi.germenji@gmail.com>
2021-01-13 17:58:26 +0100

    add js

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 'use strict';
 console.log('hi there!');
+const sel = function (element) {
+    if (element.charAt(0) === '#') {
+        // If passed an ID...
+        return document.querySelector(element); // ... returns single element
+    }
+    return document.querySelectorAll(element); // Otherwise, returns a nodelist
+};
+
+const secondHand = document.querySelector('.second-hand');
+console.log(secondHand);
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDeg = (seconds / 60) * 360 + 90;
+    secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDeg = (minutes / 60) * 360 + 90;
+    minuteHand.style.transform = `rotate(${minutesDeg}deg)`;
+
+    const hours = now.getHours();
+    const hoursDeg = (hours / 12) * 360 + 90;
+    hourHand.style.transform = `rotate(${hoursDeg}deg)`;
+
+    console.log(seconds);
+    console.log(secondsDeg);
+}
+
+setInterval(setDate, 1000);
